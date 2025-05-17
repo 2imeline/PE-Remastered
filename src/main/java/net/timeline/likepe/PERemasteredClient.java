f68caa4d4550a178b6ede52604d87afa98476cc5
time <121706381+2imeline@users.noreply.github.com>
2025-05-16 21:26:27 -0400

    Initial commit

---
+++ src/main/java/net/timeline/likepe/PERemasteredClient.java
@@ -0,0 +1,10 @@
+package net.timeline.likepe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PERemasteredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/timeline/likepe/PERemasteredDataGenerator.java
@@ -0,0 +1,11 @@
+package net.timeline.likepe;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PERemasteredDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
